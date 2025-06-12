fix: update loss calculation

src/model.py
@@ -8,16 +8,14 @@ import torch.nn as nn
 class MLPOutput:
 
     y: torch.Tensor
-    loss: torch.Tensor
 
 
 class MLP(nn.Module):
 
-    def __init__(self, layers: list[nn.Module], loss_function: nn.Module):
+    def __init__(self, layers: list[nn.Module]):
         super(MLP, self).__init__()
 
         self.layers = nn.Sequential(*layers)
-        self.loss_function = loss_function
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.to(self.device)
@@ -26,9 +24,4 @@ class MLP(nn.Module):
 
         y = self.layers(x)
 
-        # `loss_function(z, x)` computes tensor of shape [z, x] with element-wise loss
-        # `.sum(-1)` sums loss over predicted features (total loss per sample)
-        # `.mean()` averages the per sample loss over batch
-        loss = self.loss_function(y, x).sum(-1).mean()
-
-        return MLPOutput(y=y, loss=loss)
+        return MLPOutput(y=y)

src/training.py
@@ -92,6 +92,7 @@ class TrainingOutput:
 def train(
     model: nn.Module,
     optimizer: optim.Optimizer,
+    criterion: nn.Module,
     train_loader: DataLoader,
     epoch: int,
     train_output: TrainingOutput,
@@ -115,8 +116,10 @@ def train(
 
         optimizer.zero_grad()  # Clear gradients
         output = model(data)
-        # Average batch loss stored in output
-        loss = output.loss
+        # `loss_function(z, x)` computes tensor of shape [z, x] with element-wise loss
+        # `.sum(-1)` sums loss over predicted features (total loss per sample)
+        # `.mean()` averages the per sample loss over batch
+        loss = criterion(output.y, target).sum(-1).mean()
         loss.backward()  # Backpropagation
         optimizer.step()  # Update parameters
 
@@ -131,6 +134,7 @@ def train(
 
 def validate(
     model: nn.Module,
+    criterion: nn.Module,
     val_loader: DataLoader,
     epoch: int,
     train_output: TrainingOutput,
@@ -154,7 +158,7 @@ def validate(
             data, target = data.to(device), target.to(device)
 
             output = model(data)
-            loss = output.loss
+            loss = criterion(output.y, target).sum(-1).mean()
 
             # Sum average batch loss
             val_loss += loss
@@ -177,6 +181,7 @@ def test(model: nn.Module, test_loader: DataLoader, device: str, logger: Logger)
             total=len(test_loader),
             desc=f"Testing",
         ):
+            logger.info(f"Testing BATCH {batch_idx + 1} / {len(test_loader)}")
             data, target = data.to(device), target.to(device)
 
             output = model(data)

src/utils/configurator.py
@@ -60,12 +60,10 @@ class ModelConfig:
     type: str
     n_layers: int
     raw_layers: list
-    loss: nn.Module
     layers: list = field(init=False)
 
     def __post_init__(self):
         super().__setattr__("layers", self.process_layers(self.type, self.raw_layers))
-        self.loss = torch_loss[self.loss]
 
     def process_layers(self, type, raw_layers):
 
@@ -96,9 +94,11 @@ class TrainingConfig:
     batch_size: int
     epochs: int
     optimizer: optim.Optimizer
+    loss: nn.Module
 
     def __post_init__(self):
         self.optimizer = torch_optim[self.optimizer]
+        self.loss = torch_loss[self.loss]
 
 
 def load_config_yml(path_to_file: str) -> dict: