969920feab385d5942e60e1966ad5c1de53c423e
ChrisKoll <christian.kolland@web.de>
2025-07-02 21:48:44 +0200

    feat: add saving of metrics to test run

--- src/fit/fit_model.py
+++ src/fit/fit_model.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 import logging
+from pathlib import Path
 import sys
 import time
 
@@ -142,7 +143,7 @@ def test_model_fitting():
     formatted_time = str(timedelta(seconds=int(elapsed_time)))
     logger.info(f"Model fitting took: {formatted_time} (HH:MM:SS)")
 
-    pipeline.save_training_results(best_model, metrics, "./test_run")
+    pipeline.save_training_results(best_model, metrics, (Path"./test_run"))
 
 
 def fit_model(path_to_setup: str, path_to_out: str):
