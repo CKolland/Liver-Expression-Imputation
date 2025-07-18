b0a4ae19439527c6a58baeccb7d1098bebf2820e
ChrisKoll <christian.kolland@web.de>
2025-07-18 16:36:22 +0200

    feat: improve io

--- src/small_script.py
+++ src/small_script.py
@@ -12,14 +12,18 @@ def main():
     args = parser.parse_args()
 
     adata = ad.read_h5ad(args.file)
+    print(adata)
 
     if args.type == 0:
         df = pd.read_feather("./whole_transcriptome.feather")
         adata.uns["target_names"] = df[0].to_list()
+        print("Transcriptome genes added.")
     elif args.type == 1:
         df = pd.read_feather("./ST_genes.feather")
         adata.uns["target_names"] = df[0].to_list()
+        print("ST genes added.")
 
+    print(adata)
     adata.write(args.file, compression="gzip")
 
 
