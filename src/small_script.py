72d90c934fea7e7737a76e8b8dbcc7b41f50a5b5
ChrisKoll <christian.kolland@web.de>
2025-07-22 16:41:21 +0100

    feat: add seed test configs

--- src/small_script.py
+++ src/small_script.py
@@ -14,11 +14,11 @@ def main():
     adata = ad.read_h5ad(args.file)
     print(adata)
 
-    if args.type == 0:
+    if args.type == "0":
         df = pd.read_feather("./whole_transcriptome.feather")
         adata.uns["target_names"] = df[0].to_list()
         print("Transcriptome genes added.")
-    elif args.type == 1:
+    elif args.type == "1":
         df = pd.read_feather("./ST_genes.feather")
         adata.uns["target_names"] = df[0].to_list()
         print("ST genes added.")
