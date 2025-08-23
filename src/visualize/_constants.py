fdafc455f3885f3db86616ea357c8ce60ff89103
ChrisKoll <christian.kolland@web.de>
2025-08-23 14:08:41 +0200

    fix: update constant names

--- src/visualize/_constants.py
+++ src/visualize/_constants.py
@@ -35,19 +35,19 @@ TEST_MASKS_HELP = "Path to data frame that contains the masks."
 # ------------------------
 
 # Command parser setup
-SUB_COMMAND_TEST = "treshold"
-TEST_HELP = (
+SUB_COMMAND_TRESHOLD = "treshold"
+TRESHOLD_HELP = (
     "Calculate gene-wise test metrics with applied threshold to predicted values."
 )
 
-TEST_ADATA_LONG = "--adata"
-TEST_ADATA_SHORT = "-d"
-TEST_ADATA_HELP = "Paths to AnnData (.h5ad) file."
-TEST_MASKS_LONG = "--masks"
-TEST_MASKS_SHORT = "-m"
-TEST_MASKS_DEFAULT = None
-TEST_MASKS_HELP = "Path to data frame that contains the masks."
-TEST_MASKS_LONG = "--threshold"
-TEST_MASKS_SHORT = "-t"
-TEST_MASKS_DEFAULT = 0.05
-TEST_MASKS_HELP = "Threshold that is applied to predicted values. Defaults to 0.05."
+TRESHOLD_ADATA_LONG = "--adata"
+TRESHOLD_ADATA_SHORT = "-d"
+TRESHOLD_ADATA_HELP = "Paths to AnnData (.h5ad) file."
+TRESHOLD_MASKS_LONG = "--masks"
+TRESHOLD_MASKS_SHORT = "-m"
+TRESHOLD_MASKS_DEFAULT = None
+TRESHOLD_MASKS_HELP = "Path to data frame that contains the masks."
+TRESHOLD_MASKS_LONG = "--threshold"
+TRESHOLD_MASKS_SHORT = "-t"
+TRESHOLD_MASKS_DEFAULT = 0.05
+TRESHOLD_MASKS_HELP = "Threshold that is applied to predicted values. Defaults to 0.05."
